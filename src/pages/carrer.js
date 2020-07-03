7b2d893d99f68b3e05c94da3a0dcb4f4b3884343
Rehan Mohiuddin <rehan.4942@gmail.com>
2020-07-03 19:37:18 +0530

    updatess1

--- src/pages/carrer.js
+++ src/pages/carrer.js
@@ -10,6 +10,7 @@ const Carrer = () => {
              <h1 style={{textAlign: `center`, marginBottom: `40px`}}>Carrers At HT GLOBAL SYSTEMS</h1>
             <div className="page-content">
                 <MDBCard style={{backgroundColor:"#fffff"}}>
+                <Markdown source={aboutText[0].title} escapeHtml={false} />
                     <MDBCardText>
                     <Markdown source={aboutText[0].content} escapeHtml={false} />
                     </MDBCardText>
