2832d353da5bac561ca4aaec87d72c0c7dd8d47f
Rehan Mohiuddin <rehan.4942@gmail.com>
2020-07-03 20:06:28 +0530

    updatess4

--- public/main.js
+++ public/main.js
@@ -110,34 +110,8 @@ const getPages = () => {
         files.forEach((file, i) => {
             let page
             fs.readFile(`${dirPathPages}/${file}`, "utf8", (err, contents) => { 
-                const getMetadataIndices = (acc, elem, i) => {
-                    if (/^---/.test(elem)) {
-                        acc.push(i)
-                    }
-                    return acc
-                }
-                const parseMetadata = ({lines, metadataIndices}) => {
-                    if (metadataIndices.length > 0) {
-                        let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1])
-                        metadata.forEach(line => {
-                            obj[line.split(": ")[0]] = line.split(": ")[1]
-                        })
-                        return obj
-                    }
-                }
-                const parseContent = ({lines, metadataIndices}) => {
-                    if (metadataIndices.length > 0) {
-                        lines = lines.slice(metadataIndices[1] + 1, lines.length)
-                    }
-                    return lines.join("\n")
-                }
-                const lines = contents.split("\n")
-                const metadataIndices = lines.reduce(getMetadataIndices, [])
-                const metadata = parseMetadata({lines, metadataIndices})
-                const content = parseContent({lines, metadataIndices})
                 page = {
-                    title: metadata.title ? metadata.title : "No title given",
-                    content: content ? content : "No content given",
+                    content: contents
                 }
                 pagelist.push(page)
                 let data = JSON.stringify(pagelist)
@@ -156,34 +130,8 @@ const getPage = () => {
         files.forEach((file, i) => {
             let page
             fs.readFile(`${dirPathPage}/${file}`, "utf8", (err, contents) => { 
-                const getMetadataIndices = (acc, elem, i) => {
-                    if (/^---/.test(elem)) {
-                        acc.push(i)
-                    }
-                    return acc
-                }
-                const parseMetadata = ({lines, metadataIndices}) => {
-                    if (metadataIndices.length > 0) {
-                        let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1])
-                        metadata.forEach(line => {
-                            obj[line.split(": ")[0]] = line.split(": ")[1]
-                        })
-                        return obj
-                    }
-                }
-                const parseContent = ({lines, metadataIndices}) => {
-                    if (metadataIndices.length > 0) {
-                        lines = lines.slice(metadataIndices[1] + 1, lines.length)
-                    }
-                    return lines.join("\n")
-                }
-                const lines = contents.split("\n")
-                const metadataIndices = lines.reduce(getMetadataIndices, [])
-                const metadata = parseMetadata({lines, metadataIndices})
-                const content = parseContent({lines, metadataIndices})
                 page = {
-                    title: metadata.title ? metadata.title : "No title given",
-                    content: content ? content : "No content given",
+                    content: contents
                 }
                 pagelists.push(page)
                 let data = JSON.stringify(pagelists)
